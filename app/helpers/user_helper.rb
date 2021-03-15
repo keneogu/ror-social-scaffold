045c33c8952d3f47436216cfcae479692e093e62
KumarAmitt <amitt2015@outlook.com>
2021-03-15 18:06:05 +0530

    Implement methods for user_helper

---
+++ app/helpers/user_helper.rb
@@ -0,0 +1,11 @@
+module UserHelper
+
+  def profile_heading(user)
+    (current_user == user) ? "My Profile: #{user.name}" : "Name: #{user.name}"
+  end
+
+  def show_invite_to_friendship(user)
+    invite_to_friendship(user) unless current_user == user
+  end
+
+end
