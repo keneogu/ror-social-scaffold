a525780ee3ea5a82c9bd4d00f69cd97e050095e6
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-03-15 10:49:35 +0100

    Add friendship controller actions

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,11 @@
+class FriendshipsController < ApplicationController
+  def create
+	@friendship = current_user.friendships.new(friend_id: params[:user_id], confirmed: false)
+
+	if @friendship.save
+	redirect_to users_path, notice: 'Invitation sent!'
+	else
+	redirect_to users_path, alert: 'Something went wrong, please try again!'
+	end
+  end
+end
