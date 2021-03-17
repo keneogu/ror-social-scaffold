304b2b1630cd98115c5c84ec55a3f5acccaccd8e
KumarAmitt <amitt2015@outlook.com>
2021-03-17 13:20:49 +0530

    Refactor code

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -7,7 +7,15 @@ module UserHelper
     invite_to_friendship(user) unless current_user == user
   end
 
+  def mutual_friends_heading(user)
+    'Mutual Friends:' if user != current_user
+  end
+
+  def no_mutual_friends(user)
+    "You and #{user.name} have no common friends." if @mutual_friends.size.zero?
+  end
+
   def mutual_friends_list(user)
-    @mutual_friends if user != current_user && @mutual_friends.size.positive?
+    user != current_user ? @mutual_friends : []
   end
 end
